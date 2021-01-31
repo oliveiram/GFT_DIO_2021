a3c8c3d75a75a18f38a32a18c875f49af669e578
Márcio de Jesus <marciohost@gmail.com>
2021-01-31 20:07:30 -0300

    Forth Commit

--- js_aula_1/js/main.js
+++ js_aula_1/js/main.js
@@ -1,1 +1,12 @@
-alert('meu primeiro js');
+
+function soma(n1, n2){
+    return n1+n2;
+}
+
+alert(soma(8, 15));
+
+/*
+var nome = "Márcio de Jesus";
+var idade = 37
+alert(nome+" tem "+idade+" anos");
+*/

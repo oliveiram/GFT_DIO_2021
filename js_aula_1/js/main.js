aa89ec04f06c7c51376fdcce2e8207e9e563ae01
Márcio de Jesus <marciohost@gmail.com>
2021-02-02 19:49:23 -0300

    onlick, onmouseover e onmouseout

--- js_aula_1/js/main.js
+++ js_aula_1/js/main.js
@@ -1,11 +1,30 @@
+function clicou(){
 
+   //window.document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+   window.document.getElementById("redirecionar").innerHTML = "Obrigado por clicar";
+    //alert("Obrigado por clicar");
+}
+function redirecionar(){
+    //window.open("https://globallab.org/en/#.YBnQ2uhKiUk");
+    window.location.href = "https://globallab.org/en/#.YBnQ2uhKiUk";
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+/*
 function soma(n1, n2){
     return n1+n2;
 }
 
 alert(soma(8, 15));
 
-/*
+
 var nome = "Márcio de Jesus";
 var idade = 37
 alert(nome+" tem "+idade+" anos");

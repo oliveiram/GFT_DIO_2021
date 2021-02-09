66c6fa6f01acbeaec5858273168e4b0a812acdd1
Márcio de Jesus <marciohost@gmail.com>
2021-02-08 21:53:59 -0300

    Arrow Functions

---
+++ JS_es6_Advanced/aula1/arrowFunctions.js
@@ -0,0 +1,15 @@
+/* var sumOld = function(a,b){
+    return a+b;
+}
+
+var sum = (a,b) => a+b;
+console.log(sum(5,8));
+
+var createObj = () => ({text: 123});
+console.log(createObj()); */
+
+function car (){
+    this.foo = 'bar'
+}
+console.log(new car()); 
+
